d61f233aca51250f4b0646df06d860b528502226
185061815124 <1550846713@qq.com>
2021-03-15 04:47:31 +0800

    ascii格式打印

---
+++ [lie13.2].c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+	FILE* fp = fopen("C:\\Users\\Moqilin\\Desktop\\test_git\\185061815124533.github.io\\02.txt", "wb");
+	char ch;
+	int i;
+	if (fp == NULL)
+	{
+		printf("Failure to open 02.txtx!\n");
+		exit(0);
+	}
+	for (i = 0; i < 128; i++)
+	{
+		fputc(i, fp);
+	}
+	fclose(fp);
+	FILE* fp1 = fopen("C:\\Users\\Moqilin\\Desktop\\test_git\\185061815124533.github.io\\02.txt", "rb");
+	if (fp1 == NULL)
+	{
+		printf("Failure to open 02.txtx!\n");
+		exit(0);
+	}
+	//while ((ch = fgetc(fp1)) != EOF)
+	//ch = fgetc(fp1);
+	while (!feof(fp1))
+	{
+		ch = fgetc(fp1);
+		if (isprint(ch))
+			printf("%c \t", ch);
+		else
+			printf("%d \t", ch);
+	}
+	fclose(fp1);
+	return 0;
+}
